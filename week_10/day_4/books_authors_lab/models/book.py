cce5a13808478a9845998ea002f1c499228d85da
Marcin Borowski <borowski1976@yahoo.com>
2021-09-09 11:06:10 +0100

    first commit

---
+++ week_10/day_4/books_authors_lab/models/book.py
@@ -0,0 +1,7 @@
+class Book:
+
+    def __init__(self, title, author, release_date, id = None):
+        self.title = title
+        self.author = author
+        self.release_date = release_date
+        self.id = id
